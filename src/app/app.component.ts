c66e049c2b4111c1a68395f42ac2647aa7203b03
Ram Achal <ram.0436@gmail.com>
2023-10-16 01:57:27 +0530

    title message modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Claxified';
+  title = 'Free Classified in India | Search Engine for Ads | Claxified.in';
 }
