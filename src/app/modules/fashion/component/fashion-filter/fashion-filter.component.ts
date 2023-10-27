cd5a6fad44ee8d37dcd7962aea8ebb712b745b31
Hamzaweb49 <hamzaazam1234567@gmail.com>
2023-10-27 18:28:39 +0500

    Made modules responsive

--- src/app/modules/fashion/component/fashion-filter/fashion-filter.component.ts
+++ src/app/modules/fashion/component/fashion-filter/fashion-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, HostListener, EventEmitter, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocomplete } from '@angular/material/autocomplete';
 import { MatSelectionList } from '@angular/material/list';
@@ -13,6 +13,7 @@ import { CommonService } from 'src/app/shared/service/common.service';
   styleUrls: ['./fashion-filter.component.css', '../../../modulefilter.component.css']
 })
 export class FashionFilterComponent {
+  @Output() resetClicked: EventEmitter<void> = new EventEmitter<void>();
 
   stateControl = new FormControl("");
   cityControl = new FormControl("");
@@ -213,7 +214,13 @@ export class FashionFilterComponent {
     this.filterObj = { ...this.initialFilters }; // Reset to initial filters
     this.appliedFilters = [];
     window.location.reload();
+    this.resetClicked.emit();
   }
+
+  closeFilters(){
+    this.resetClicked.emit();
+  }
+
 
 
   resetFilters() {
