eb03377f0b2d5cf4b329c7fd51e91d107382bf2b
Ram Achal <ram.0436@gmail.com>
2026-05-13 14:19:22 +0530

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'https://www.claxified.com | Free Classified Ads in India | Sell Anything Online';
+  title = 'Free Classified Ads in India | Sell Anything Online';
 }
