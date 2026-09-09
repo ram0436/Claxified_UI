e33626f9e95a205f00743257a2316be36b7f4a2f
Ram Achal <ram.0436@gmail.com>
2026-09-09 22:39:01 +0530

    title modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { filter } from "rxjs/operators";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "Claxified - Post Free Ads | Sell Anything Find Everything";
+  title = "Claxified - List Your Business | Events | Free Classified Ads";
 
   showCommonHeader = true;
 
